9c4f75a3082915b7b7a8ca86dae1556c4334b9fb
awebmekhilef <awebmekhilef@gmail.com>
2021-01-04 14:42:04 +0800

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const io = require('socket.io')(5000)
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server)
+const path = require('path');
 
 const wordlist = require('./wordlist')
+
+server.listen(process.env.PORT || 5000)
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // {id, username, score}
 let players = []
